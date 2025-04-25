a5205f0d96409fddde0bcca39b99febb6105aef6
niu <15955434043@163.com>
2025-04-25 14:03:31 +0800

    Initial commit

---
+++ src/main/java/space/nbtca/McpServerJavaApplication.java
@@ -0,0 +1,13 @@
+package space.nbtca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/nbtca/McpServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package space.nbtca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
